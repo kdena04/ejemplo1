3f08b82f93fa5f8ce24910b88313ebb5f6a85840
kdena04 <jonathankdena20444@gmail.com>
2025-02-05 21:35:45 -0600

    listo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,75 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
+  styleUrl: './app.component.css'}
+)
 export class AppComponent {
   title = 'fundamentos';
+
+  //propiedades 
+
+  nombre= 'Alejandro';
+  email= 'acvalencia@utsjr.edu.mx';
+  aniosServicio= 22;
+  activo=false;
+  alumnos=[24,25,26];
+  numero = 5;
+  contador= 0;
+  desactivarBoton = true;
+
+  productos = [
+
+    {
+      id: 1,
+      descripcion: 'Cocada',
+      precio: 15.15
+    },
+    {
+      id: 2,
+      descripcion: 'Sabritas',
+      precio: 20.50
+    },
+    {
+      id: 3,
+      descripcion: 'Manguito',
+      precio: 11
+    }
+
+  ];
+
+  //funcion que muestra si el profesor esta activo o no
+  estaActivo(){
+    if(this.activo){
+      return 'Empleado activo';
+
+    }else{
+      return'Empleado inactivo';
+    }
+  };
+  
+  //funcion que suma los alumnos del arreglo
+  sumarAlumnos(){
+    let suma =0;
+    for(let i=0; i<this.alumnos.length;i++){
+      suma += this.alumnos[i];
+    }
+    return suma;
+  };
+
+  //funcion que incrementa el contador
+  incrementarContador(){
+    this.contador++;
+  };
+
+  //funcion que decrementa el contador
+  decrementarContador(){
+    this.contador--;
+  };
+
+  //funcion que cambia el valor de desactivar boton
+  cambiarValor(){
+    if(this.desactivarBoton){
+      this.desactivarBoton= false;
+    }
+  }
 }
